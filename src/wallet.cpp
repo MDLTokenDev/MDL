c0fe628eb9778c7bcf4c83c86e6d0f0afd3d1c89
MDLTokenDev <67667490+MDLTokenDev@users.noreply.github.com>
2020-07-01 17:33:12 +0200

    v1.0.0

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -2782,9 +2782,9 @@ bool CWallet::CreateTransaction(const vector<pair<CScript, CAmount> >& vecSend,
                     if (coin_type == ALL_COINS) {
                         strFailReason = _("Insufficient funds.");
                     } else if (coin_type == ONLY_NOTDEPOSITIFMN) {
-                        strFailReason = _("Unable to locate enough funds for this transaction that are not equal to 150000 mdl.");
+                        strFailReason = _("Unable to locate enough funds for this transaction that are not equal to 100000 mdl.");
                     } else if (coin_type == ONLY_NONDENOMINATED_NOTDEPOSITIFMN) {
-                        strFailReason = _("Unable to locate enough Obfuscation non-denominated funds for this transaction that are not equal to 150000 mdl.");
+                        strFailReason = _("Unable to locate enough Obfuscation non-denominated funds for this transaction that are not equal to 100000 mdl.");
                     } else {
                         strFailReason = _("Unable to locate enough Obfuscation denominated funds for this transaction.");
                         strFailReason += " " + _("Obfuscation uses exact denominated amounts to send funds, you might simply need to anonymize some more coins.");
